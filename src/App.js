2101e14a03b4f472b16331895842ca4c89352c79
gunasundarigithub <divyaeee1202@gmail.com>
2020-01-24 00:25:07 +0530

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './SearchAppBar.js'
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
+
+class App extends React.Component{
+
+const useStyles = makeStyles(() => ({
+  root: {
+    marginTop: '5px',
+    alignItems: 'center',
+  },
+}));
+ 
+ render(){
+
+const team_list = [
+  { team_name:'team-1', team_id: 1994 },
+  { team_name: 'team-2', team_id: 1972 },
+  { team_name: 'team-3', team_id: 1974 },
+  { team_name: 'team-4', team_id: 2008 },
+];
+
+
+return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <SearchAppBar/>
+      <br/><br/>
+      <div className='root'>
+      <Autocomplete display="inline"
+      style={{ width: 400}}
+      options={team_list.map(option => option.team_name)}
+        renderInput={params => (
+          <TextField {...params} label="Enterprise_teams" color="secondary"  variant='outlined' style={{ width: 300}} />
+        )}
+  /> <Button variant="contained" display='inline' color="secondary">
+  Create/edit/view schedule
+</Button>
+  </div>
+</div>
+     );
+}
 }
 
 export default App;
