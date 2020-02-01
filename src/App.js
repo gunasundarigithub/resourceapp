c049fcdf271c35552d5d57294cce473be53304b3
gunasundarigithub <divyaeee1202@gmail.com>
2020-02-02 03:12:15 +0530

    ui_done_again

--- src/App.js
+++ src/App.js
@@ -6,51 +6,47 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Button from '@material-ui/core/Button';
 import Table from './Components/Table';
-
+import Select from './Components/MaterialUI/Select';
 
 class App extends React.Component{
 
 state= {
-  showTable:false,
-  showbutton:false,
+  showTable:false, //to show the table after clicking on the button
+  showButton:false, //to show the button after clicking on the team from the drop down
+  selectedTeam: 'Ent_digital', //selected value from the drop down
+  
 
     }
 
-handleChange = (e) => {
-    this.setState({ renderInput: e.target.value,showbutton: true }, () => console.log("hiiiiiii",this.state));
+handleChange = (event,value) => {
+  console.log(event.target.value);
+  console.log(event)
+  console.log(value)
+    this.setState({ selectedTeam: event.target.value,showButton: true });
   }
 
 render(){
 
 const team_list = [
-  { team_name:'team-1', team_id: 1 },
-  { team_name: 'team-2', team_id: 2 },
-  { team_name: 'team-3', team_id: 3 },
-  { team_name: 'team-4', team_id: 4 },
+  { team_name:'Ent_digital', team_id: 1 },
+  { team_name: 'Auto_Exp', team_id: 2 },
+  { team_name: 'ECT', team_id: 3 },
+  { team_name: 'EFT', team_id: 4 },
 ];
-
-
-
-
- //const [showTable, setShowTable] = useState(false);
+//const [showTable, setShowTable] = useState(false);
 
 return (
   <div className="App">
-      <SearchAppBar/>
-      <br/><br/>  
-      <div style={{ display: 'inline-flex' }}>
-      <div> <Autocomplete display="inline"
-      style={{ width: 350, float :"left"}}
-      options={team_list.map(option => option.team_name)}
-      onChange={this.handleChange}
-        renderInput={params => (
-
-          <TextField {...params} label="Enterprise_teams" color="secondary"  variant='outlined' style={{ width: 300}} />)}/> 
-  <Button variant="contained" display='inline' color="secondary" disabled={!(this.state.value)}
-  onClick={() => {this.setState({showTable:true})} }>
-  Create/edit/view schedule
-</Button>
-    </div></div>
+      <SearchAppBar/> 
+      <div style={{  display: 'inline-flex' }}>
+      <div>
+      <br></br>
+      <Select title="Enterprise Teams" change={this.handleChange} items={team_list} selectedName={this.state.selectedTeam}   />
+    { this.state.showButton ? <Button variant="contained" display='inline'  color="secondary" onClick={() => {this.setState({showTable:true})} }>
+       Create/edit/view schedule
+     </Button> :null }
+    </div>
+    </div>
 {this.state.showTable ? <div><Table></Table></div> : null }
 </div>
      );
---
+++ src/Components/MaterialUI/Select.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+  }
+}));
+
+export default function SimpleSelect(props) {
+  const classes = useStyles();
+  
+
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
+
+
+  return (
+    <div>
+     
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel ref={inputLabel} id="demo-simple-select-outlined-label">
+          {props.title}
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={props.selectedName}
+          onChange={props.change}
+          labelWidth={labelWidth}
+        >
+
+{console.log("test")}
+{props.items.map((arrayvalue)=>{
+ 
+ const tname=arrayvalue.team_name;
+ const tid=arrayvalue.team_id;
+console.log(tname,tid);
+
+return <MenuItem value={tid}>{tname}</MenuItem>
+
+//return (console.log(arrayvalue));})
+    })}
+
+          {/* <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Team</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem> */}
+        </Select>
+      </FormControl>
+         </div>
+  );
+}
