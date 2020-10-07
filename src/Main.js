f36683bb2bde353e077926d4396961e9e6a003aa
gunasundarigithub <divyaeee02@gmail.com>
2020-10-06 22:21:27 -0700

    trying to get the data

--- src/Main.js
+++ src/Main.js
@@ -5,6 +5,7 @@ import Button from '@material-ui/core/Button';
 import Table from './Components/Table';
 import Select from './Components/MaterialUI/Select'; 
 import SelectEditor from './Components/SelectEditor';
+import axios from './Axios/axios'
 
 
 class App extends React.Component{
@@ -15,6 +16,7 @@ state= {
   showTable:false, //to show the table after clicking on the button
   showButton:false, //to show the button after clicking on the team from the drop down
   selectedTeam: '', //selected value from the drop down
+  team_list:[]
    }
 
 handleChange = (event,value) => {
@@ -24,15 +26,27 @@ handleChange = (event,value) => {
     this.setState({ selectedTeam: event.target.value,showButton: true });
   }
 
+
+ componentDidMount(){
+   axios.get('/getteam').then(res=>{
+     //this.setState({selectedTeam:res.data});
+     console.log("connected server",res.data.length);
+      let team_list=[]
+     for(var i=0;i< res.data.length;i++){
+     team_list=res.data[i]
+     console.log("team_list",team_list)
+     this.setState({team_list:team_list})
+     }
+   })
+ } 
+
 render(){
 
-
-
 const team_list = [
-  { team_name:'Ent_digital', team_id: 1 },
-  { team_name: 'Auto_Exp', team_id: 2 },
-  { team_name: 'ECT', team_id: 3 },
-  { team_name: 'EFT', team_id: 4 },
+ { team_name:'wmma', team_id: 1 },
+ { team_name: 'Auto_Exp', team_id: 2 },
+ { team_name: 'ECT', team_id: 3 },
+ { team_name: 'EFT', team_id: 4 },
 ];
 //const [showTable, setShowTable] = useState(false);
 
